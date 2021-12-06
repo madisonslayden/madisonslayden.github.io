5eb805311890121c1c7331d6bf9afabf80975276
madisonslayden <mslayden123@gmail.com>
2021-12-06 15:35:39 +0000

    add collectables

--- projects/platformer/js/init/collectable.js
+++ projects/platformer/js/init/collectable.js
@@ -35,11 +35,10 @@
 
         ////////////////////////////////////////////////////////////////////////
         // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
-        
-        // example: 
-        createCollectable(type.steve, 200, 170, 6, 0.7);
-        
-        
+                 
+        createCollectable(type.grace, 550, 50, 6, 0.7);
+        createCollectable(type.max,700,250,6,0.7);
+        createCollectable(type.db,400,350,6,0.7,);
         
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////
